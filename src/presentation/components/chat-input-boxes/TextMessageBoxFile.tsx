da12715e7dc6567a229adf5d917f06dcc5884a62
Franco Inostroza <franco.inostrozah@gmail.com>
2024-06-15 23:30:59 -0400

    :sparkles: added audio to text view

--- src/presentation/components/chat-input-boxes/TextMessageBoxFile.tsx
+++ src/presentation/components/chat-input-boxes/TextMessageBoxFile.tsx
@@ -1,13 +1,13 @@
 import { FormEvent, useRef, useState } from "react";
 
 interface Props {
-  onSendMessage: (message: string) => void;
+  onSendMessage: (message: string, file: File) => void;
   placeholder?: string;
   disableCorrections?: boolean;
   accept?: string;
 }
 
-export const TextMessageBoxFile = ({ onSendMessage, placeholder, disableCorrections = false, accept }: Props) => {
+export const TextMessageBoxFile = ({ onSendMessage, placeholder, disableCorrections = false, accept, }: Props) => {
   const [message, setMessage] = useState('')
   const [selectedFile, setSelectedFile] = useState<File | null>()
   const inputFileRef = useRef<HTMLInputElement>(null);
@@ -16,10 +16,12 @@ export const TextMessageBoxFile = ({ onSendMessage, placeholder, disableCorrecti
   const handleSendMessage = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (message.trim().length === 0) return;
+    if (!selectedFile) return
+    // if (message.trim().length === 0) return;
 
-    onSendMessage(message);
+    onSendMessage(message, selectedFile);
     setMessage('');
+    setSelectedFile(null);
   }
 
   return (
--- src/presentation/pages/audio-to-text/AudioToTextPage.tsx
+++ src/presentation/pages/audio-to-text/AudioToTextPage.tsx
@@ -1,6 +1,78 @@
+import { useState } from "react"
+import { GptMessage, MyMessage, TextMessageBoxFile, TypingLoader } from "../../components";
+import { audioToTextUseCase } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
 
 export const AudioToTextPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string, audioFile: File) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text, isGpt: false }]);
+
+    const response = await audioToTextUseCase(audioFile, text);
+    console.log(response);
+
+    if (!response) return;
+
+    const gptMessage = `
+      ## Transcription:
+      __Duration__ ${Math.round(response.duration)} seconds
+      ### Text:
+      ${response.text}
+    `;
+
+    setMessages((prev) => [...prev, { text: gptMessage, isGpt: true }]);
+
+    for (const segment of response.segments) {
+      const segmentMessage = `
+        __From ${Math.round(segment.start)} to ${Math.round(segment.end)} seconds:__
+        ${segment.text}
+      `;
+
+      setMessages((prev) => [...prev, { text: segmentMessage, isGpt: true }]);
+    }
+
+
+    setIsLoading(false);
+  }
+
   return (
-    <div>AudioToTextPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/** */}
+          <GptMessage text="Hello, what audio would you like to generate today?" />
+
+          {
+            messages.map((message, index) => (
+              message.isGpt ? (<GptMessage key={index} text={message.text} />)
+                : (<MyMessage key={index} text={message.text === '' ? 'Transcribe the audio.' : message.text} />)))
+          }
+
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-1 fade-in">
+                <TypingLoader />
+              </div>
+            )
+          }
+
+        </div>
+      </div>
+
+      <TextMessageBoxFile
+        onSendMessage={handlePost}
+        placeholder="Escribe aquí lo que deseas"
+        disableCorrections
+        accept="audio/*"
+      />
+
+    </div>
   )
 }
